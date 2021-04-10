157f36b82f66bcdf86b3c889fde429a1eafe47b9
Enot1she <enotisheRND@yandex.ru>
2021-04-10 18:31:47 +0300

    добавили старую версию

--- app/src/main/java/com/example/test_git/MainActivity.java
+++ app/src/main/java/com/example/test_git/MainActivity.java
@@ -1,15 +1,43 @@
 package com.example.test_git;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-    //кирил лох
-    int a = 12;
+
+    Button btg;
+    String[] valCountries = { "1", "2", "3", "4", "5", "6", "7", "8" };
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner1);
+        // Создаем адаптер ArrayAdapter с помощью массива строк и стандартной разметки элемета spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, valCountries);
+        // Определяем разметку для использования при выборе элемента
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Применяем адаптер к элементу spinner
+        spinner.setAdapter(adapter);
+
+        btg = (Button) findViewById(R.id.btg);
+        btg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, game.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
