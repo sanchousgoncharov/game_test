8727d1ac215821df3929997e71ec795082f42e45
HUAWEI <sanchous2810@gmail.com>
2021-04-10 18:19:03 +0300

    rthtk

--- app/src/main/java/com/example/test_git/MainActivity.java
+++ app/src/main/java/com/example/test_git/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    //кирил лох
     int a = 12;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
